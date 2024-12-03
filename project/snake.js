8ad7ab0e4ba60da4f67eb2e6ed63b17f330e3b78
Koltai Marci <koltai.marton.balazs@students.jedlik.eu>
2024-12-03 20:59:51 +0100

    Snake moves (#2)

--- project/snake.js
+++ project/snake.js
@@ -1,17 +1,17 @@
 let blockSize = 33.33;
 let rowCount = 15;
 let columnCount = 15
-let canvas; 
+
 let context;
 
 
-let snakeX = blockSize * 5;
-let snakeY = blockSize * 5;
+let snakeX = blockSize * 0;
+let snakeY = blockSize * 0;
 
 let speedX = 0;
 let speedY = 0;
 
-let snakeBody = []
+let snakeBody = [[0,0]]
 
 
 
@@ -23,15 +23,18 @@ let gameOver = false;
 
 let snakeColor = 'white'
 let food = 'apple.jpeg'
+
 window.onload = function () {
     canvas = document.getElementById("gameCanvas");
-    canvas.style.width = rowCount * blockSize ;
-    canvas.style.height = columnCount * blockSize;
+    canvas.setAttribute('width', columnCount * blockSize);
+    canvas.setAttribute('height', rowCount * blockSize);
+    canvas.style.width = columnCount * blockSize
+    canvas.style.height = rowCount * blockSize
     context = canvas.getContext("2d");
     placeFood();
 
     document.addEventListener("keyup", moveSnake);
-    setInterval(update, 1000 / 10);
+    setInterval(update, 2000 / 10);
 }
 
 function update() {
@@ -39,55 +42,62 @@ function update() {
         return
     }
     context.fillStyle = "#9BBA5A";
-    context.fillRect(0, 0, board.width, board.height);
-    context.drawImage(food, foodX, foodY)
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // context.drawImage(food, foodX, foodY)
+    context.fillStyle = "red"
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+    
+
 
     if (snakeX == foodX && snakeY == foodY) {
         snakeBody.push([foodX, foodY]);
         placeFood();
     }
 
-    for (let i = snakeBody.length; i > 0; i--) {
-        snakeBody[i] = snakeBody[i-1]
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX,snakeY]
-    }
+    // for (let i = snakeBody.length; i > 0; i--) {
+    //     snakeBody[i] = snakeBody[i-1]
+    // }
+
     context.fillStyle = snakeColor;
 
     snakeX += speedX * blockSize;
     snakeY += speedY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }if (snakeX < 0 || snakeX > total_col * blockSize || snakeY < 0 || snakeY > total_row * blockSize) { 
-        gameOver = true;
-        alert("Game Over");
-    }
 
     for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) { 
-            gameOver = true;
-            alert("Game Over");
-        }
+        context.fillRect(snakeBody[i][0] + snakeX, snakeBody[i][1] + snakeY, blockSize, blockSize);
+    }
+    
+    // console.log(snakeBody)
+    
+    if (snakeX < 0 || snakeX > columnCount * blockSize || snakeY < 0 || snakeY > rowCount * blockSize) { 
+         gameOver = true;
+        //  alert("Game Over");
+         console.log(snakeX)
+         return false
     }
 
+    // for (let i = 0; i < snakeBody.length; i++) {
+    //     if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) { 
+    //         gameOver = true;
+    //         // alert("Game Over");
+    //     }
+    // }
 }
 
 function moveSnake(e) {
-    if (e.code = "ArrowDown" && speedY != -1) {
-        speedX = 0;
-        speedY = -1;
-    }
-    if (e.code = "ArrowUp" && speedY != 1) {
+    if (e.code == "ArrowDown" && speedY != 1) {
         speedX = 0;
         speedY = 1;
     }
-    if (e.code = "ArrowLeft" && speedX != -1) {
+    if (e.code == "ArrowUp" && speedY != -1) {
+        speedX = 0;
+        speedY = -1;
+    }
+    if (e.code == "ArrowLeft" && speedX != -1) {
         speedX = -1;
         speedY = 0;
     }
-    if (e.code = "ArrowRight" && speedX != 1) {
+    if (e.code == "ArrowRight" && speedX != 1) {
         speedX = 1;
         speedY = 0;
     }
@@ -96,4 +106,5 @@ function moveSnake(e) {
 function placeFood() {
     foodX = Math.floor(Math.random() * columnCount) * blockSize;
     foodY = Math.floor(Math.random() * rowCount) * blockSize;
+    console.log('Food placed', foodX, ' ', foodY)
 }
